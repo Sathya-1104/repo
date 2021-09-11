cbafeb5553c8b4bf4f8c3a1c03dc7ba1ac3c20d5
Sathya-1104 <90050492+Sathya-1104@users.noreply.github.com>
2021-09-11 18:14:48 +0530

    Assignment 2

---
+++ Error.java
@@ -0,0 +1,17 @@
+
+public class Error {
+	public static void main(String [] args) {
+		char a ='s'; 
+		int s = 95;
+		
+		a=s;
+	}	 
+    
+}
+
+
+
+
+
+//In the line 5, the integer variable s is assigned to character variable a. The type conversion is not possible .
+//The variable S is constant be converted from integer to character for a local variable a.
